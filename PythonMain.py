c7af2923736984d6249ba944785bd6653fd1affd
anannyap <129117590+anannyap@users.noreply.github.com>
2023-05-25 13:38:02 -0400

    Skeleton

---
+++ PythonMain.py
@@ -0,0 +1,15 @@
+
+### Log in / create account functions go here ###
+
+### View patient functions go here ###
+
+### Add patient functions go here ###
+
+### ML stuff ###
+
+### Change patient information ###
+
+### GUI stuff ### 
+
+
+
